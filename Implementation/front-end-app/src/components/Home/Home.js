1b3b08bddf949e0e03648665600248666db1047f
arnavmital <72349832+arnavmital@users.noreply.github.com>
2023-02-23 16:42:45 +0800

    Update Home.js

--- Implementation/front-end-app/src/components/Home/Home.js
+++ Implementation/front-end-app/src/components/Home/Home.js
@@ -1,5 +1,20 @@
-const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
+import React from 'react';
+import './Home.css'
+import NavigationBar2 from '../NavigationBar/NavigationBar2';
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+              <header>
+              <NavigationBar1/>
+              </header>
+              <img src = "./CCRentals.png" alt="CCRentals"  className = "image-logo"/><br></br>
+              <footer>
+               <a to="/"><button type="button" class="btn btn-primary position-absolute w-25 p-3 top-50 start-50 translate-middle mt-5 " id="orange-btn">Compare</button></a>
+               </footer>
+            </div>
+        )
+    }
+}
+
+export default Home;
