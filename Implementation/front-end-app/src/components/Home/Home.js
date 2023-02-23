87554bfc9fa4364bf2f00f4d8185d0da950d7152
keen <kelim1221@gmail.com>
2023-02-23 17:33:27 +0800

    home

--- Implementation/front-end-app/src/components/Home/Home.js
+++ Implementation/front-end-app/src/components/Home/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Home.css'
-import NavigationBar2 from '../NavigationBar/NavigationBar2';
+import NavigationBar from '../NavigationBar/NavigationBar';
 class Home extends React.Component{
     render(){
         return(
-            <div>
-              <header>
-              <NavigationBar1/>
-              </header>
-              <img src = "./CCRentals.png" alt="CCRentals"  className = "image-logo"/><br></br>
-              <footer>
-               <a to="/"><button type="button" class="btn btn-primary position-absolute w-25 p-3 top-50 start-50 translate-middle mt-5 " id="orange-btn">Compare</button></a>
-               </footer>
+            <div className="d-grid gap-5">
+              <div>
+                <img src = "./CCRentals.png" alt="CCRentals"  className = "img-fluid mx-auto d-block margin-top"/>
+              </div>
+              
+              <div className="d-grid gap-2 col-3 mx-auto mt-5 ">
+                <button className="btn btn-success p-3 rounded-pill" type="button">Compare</button>
+                
+              </div>
+
             </div>
         )
     }
