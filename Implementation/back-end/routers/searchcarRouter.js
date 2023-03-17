0740b028e95137b23c98a51209514648f4f9691f
Lijiaqian1 <95671887+Lijiaqian1@users.noreply.github.com>
2023-03-17 22:13:58 +0800

    Update searchcarRouter.js

--- Implementation/back-end/routers/searchcarRouter.js
+++ Implementation/back-end/routers/searchcarRouter.js
@@ -37,3 +37,47 @@ router.post('/cars', async (req, res) => {
 });
 
 module.exports = searchcarRouter;
+/*
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const AudiCar = mongoose.model('carlitecars');
+const BmwCar = mongoose.model('getgocars');
+const MercedesCar = mongoose.model('tribecars');
+
+router.post('/cars', async (req, res) => {
+  var selectedTypeOfCar = req.body.typeofcar; 
+  var selectedTypeOfFuel = req.body.typeofFuel; 
+
+  var filter = {};
+  if (selectedTypeOfCar) {
+    filter.typeofcar = selectedTypeOfCar;
+  }
+  if (selectedTypeOfFuel) {
+    filter.typeofFuel = selectedTypeOfFuel;
+  }
+
+  const cars = [];
+
+  const carlitecars = await carlitecars.find({
+    available: true,
+    ...filter 
+  }).sort({ price: 1 }).exec();
+
+  const getgocars = await getgocars.find({
+    available: true,
+    ...filter 
+  }).sort({ price: 1 }).exec();
+
+  const tribecars = await tribecars.find({
+    available: true,
+    ...filter 
+  }).sort({ price: 1 }).exec();
+
+  cars.push(...audiCars, ...bmwCars, ...mercedesCars);
+
+  res.json(cars);
+});
+
+module.exports = searchcarRouter;
+*/
