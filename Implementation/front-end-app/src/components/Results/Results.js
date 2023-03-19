515c3de872589ea21b8b2635d6da2d45a0888bbb
keen <kelim1221@gmail.com>
2023-03-19 11:25:51 +0800

    Revert "search and result page"
    This reverts commit 34535d6dd00095f64ab5fa6842a862f59c21408d.

--- Implementation/front-end-app/src/components/Results/Results.js
+++ Implementation/front-end-app/src/components/Results/Results.js
@@ -8,89 +8,62 @@ import { Outlet } from 'react-router-dom';
 
 const Results = () => {
 
-  const Cars = {
-    imageSrc: '',
-    name: 'Mini Cooper',
-    location: 'Nanyang Avenue 24, 639811',
-    price: 'SGD 45',
-    noOfSeats: '4',
-    transmission: 'Automatic',
-  }
-
-
-  const CarArray = [Cars];
-
     const [selectedDate,setSelectedDate] = useState(null);
-    const [location, setLocation] = useState('');
-    const [estimateTime, setEstimateTime] = useState('');
-    const [typeofcar, setTypeofcar] = useState('');
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(location, estimateTime, typeofcar, selectedDate);
-    }
 
     return(
+    <form className="vw-100 background-darkblue text-emphasis-dark px-5 py-2">
+    <div className="row mb-3">
 
-      <div>
-     
-      <form className="vw-100 background-darkblue text-emphasis-dark px-5 py-2" onSubmit={handleSubmit}>
-      <div className="row mb-3">
-  
-      <div className="col-4">
-        <label for="Location">Pickup Location</label>
-        <input type="text" className="form-control" id="specificSizeInputName" placeholder="Jurong West Street 61"
-        value = {location} onChange={(e)=>setLocation(e.target.value)}/>
-      </div>
-  
-      
-  
-      <div className="datepicker col-4 d-inline">
-                <label for="specificSizeInputGroupUsername">Pickup Date and Time</label>
-                <Datepicker wrapperClassName='datepicker'
-                  selected={selectedDate}
-                  onChange={date => setSelectedDate(date)}
-                  dateFormat = "MM/dd/yyyy  EE hh:mm a"
-                  minDate={new Date()}
-                  showTimeSelect
-                />
-      </div>
-  
-      <div className="row">
-          <div class="col-4">
-              <label for="specificSizeSelect">Type of Car</label>
-              <select class="form-select" id="specificSizeSelect"
-                value={typeofcar} onChange={(e)=>setTypeofcar(e.target.value)}>
-                <option value="All">All</option>
-                <option value="Standard">Standard</option>
-                <option value="Hybrid">Hybrid</option>
-                <option value="Automatic">Automatic</option>
-              </select>
-          </div>
-  
-          <div className="input-group col mt-4">
-            <input type="text" class="form-control" placeholder="Estimated Duration (In hours)" aria-label="Recipient's username" aria-describedby="button-addon2"
-              value = {estimateTime} onChange={(e)=>setEstimateTime(e.target.value)}/>
-            <button class="btn btn-dark" type="button" id="button-addon2">Calculate</button>
-          </div>
-  
-      </div>
-      
-  
-      </div>
-      
-      <div className="row mt-4 justify-content-center">
-        <div className="col-auto">
-          <button type="submit" className="btn btn-primary px-5">Submit</button>
-        </div>
-      </div>
-  
-    </form>
-
-    <ResultComponent carsdata = {Cars}/>
-
+    <div className="col-4">
+      <label for="Location">Pickup Location</label>
+      <input type="text" className="form-control" id="specificSizeInputName" placeholder="Jurong West Street 61"/>
     </div>
 
+    
+    <div className="col-4">
+      <label for="specificSizeInputGroupUsername">Pickup Time</label>
+      <input type="text" className="form-control" id="specificSizeInputName" placeholder="Time"/>
+    </div>
+
+
+    <div className="datepicker col-4 d-inline">
+      <label for="specificSizeInputGroupUsername">Pickup Date</label>
+      <Datepicker 
+        selected={selectedDate}
+        onChange={date => setSelectedDate(date)}
+        dateFormat="yyyy/MM/dd"
+        minDate={new Date()}
+      />
+    </div>
+
+    <div className="row">
+      <div class="col-4">
+        <label for="specificSizeSelect">Type of Car</label>
+        <select class="form-select" id="specificSizeSelect">
+          <option selected>Type...</option>
+          <option value="1">Standard</option>
+          <option value="2">Hybrid</option>
+          <option value="3">Automatic</option>
+        </select>
+      </div>
+
+      <div className="input-group col mt-4">
+        <input type="text" class="form-control" placeholder="Estimated Duration" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+        <button class="btn btn-dark" type="button" id="button-addon2">Calculate</button>
+      </div>
+
+    </div>
+    
+
+    </div>
+    
+    <div className="row mt-4 justify-content-center">
+      <div className="col-auto">
+        <button type="submit" className="btn btn-primary px-5">Submit</button>
+      </div>
+    </div>
+
+  </form>
 )
 
 
--- Implementation/front-end-app/src/components/Search/Search.js
+++ Implementation/front-end-app/src/components/Search/Search.js
@@ -9,15 +9,6 @@ import '../Results/Results.css';
 const Search = () => {
 
       const [selectedDate,setSelectedDate] = useState(null);
-      const [location, setLocation] = useState('');
-      const [estimateTime, setEstimateTime] = useState('');
-      const [typeofcar, setTypeofcar] = useState('');
-      
-
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(selectedDate,location,estimateTime,typeofcar);
-      }
 
 
        return(
@@ -28,42 +19,44 @@ const Search = () => {
           height: '100vh'
         }} className="position-relative">
 
-          <form className="vw-100 position-absolute top-50 start-50 translate-middle background-darkblue text-emphasis-dark px-5 py-2"
-          onSubmit = {handleSubmit}>
+          <form className="vw-100 position-absolute top-50 start-50 translate-middle background-darkblue text-emphasis-dark px-5 py-2">
             <div className="row">
 
             <div className="col-4">
               <label for="Location">Pickup Location</label>
-              <input type="text" className="form-control" id="location" placeholder="Jurong West Street 61"
-              value = {location} onChange={(e)=>setLocation(e.target.value)}/>
+              <input type="text" className="form-control" id="specificSizeInputName" placeholder="Jurong West Street 61"/>
             </div>
 
+            
+            <div className="col-4">
+              <label for="specificSizeInputGroupUsername">Pickup Time</label>
+              <input type="text" className="form-control" id="specificSizeInputName" placeholder="Time"/>
+            </div>
+
+
             <div className="datepicker col-4 d-inline">
-              <label for="specificSizeInputGroupUsername">Pickup Date and Time</label>
-              <Datepicker wrapperClassName='datepicker'
+              <label for="specificSizeInputGroupUsername">Pickup Date</label>
+              <Datepicker 
                 selected={selectedDate}
                 onChange={date => setSelectedDate(date)}
-                dateFormat = "MM/dd/yyyy  EE hh:mm a"
+                dateFormat="yyyy/MM/dd"
                 minDate={new Date()}
-                showTimeSelect
               />
             </div>
 
             <div className="row">
               <div class="col-4">
                 <label for="specificSizeSelect">Type of Car</label>
-                <select class="form-select" id="specificSizeSelect"
-                value={typeofcar} onChange={(e)=>setTypeofcar(e.target.value)}>
-                  <option value="All">All</option>
-                  <option value="Standard">Standard</option>
-                  <option value="Hybrid">Hybrid</option>
-                  <option value="Automatic">Automatic</option>
+                <select class="form-select" id="specificSizeSelect">
+                  <option selected>Type...</option>
+                  <option value="1">Standard</option>
+                  <option value="2">Hybrid</option>
+                  <option value="3">Automatic</option>
                 </select>
               </div>
 
               <div className="input-group col mt-4">
-                <input type="text" class="form-control" placeholder="Estimated Duration (In hours)" aria-label="Recipient's username" aria-describedby="button-addon2"
-                value = {estimateTime} onChange={(e)=>setEstimateTime(e.target.value)}/>
+                <input type="text" class="form-control" placeholder="Estimated Duration" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                 <button class="btn btn-dark" type="button" id="button-addon2">Calculate</button>
               </div>
 
