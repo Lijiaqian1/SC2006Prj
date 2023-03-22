94b3b170aef1236848a003de2076c9e577372e65
Lijiaqian1 <95671887+Lijiaqian1@users.noreply.github.com>
2023-03-22 14:50:27 +0800

    Update search.js

--- Implementation/back-end/routers/search.js
+++ Implementation/back-end/routers/search.js
@@ -1,9 +1,7 @@
 const { spawn } = require('child_process');
-const express = require('express');
-const router = express.Router();
+const express2 = require('express');
+const router = express2.Router();
 const mongoose = require('mongoose');
-require("../models/cars");
-const Car = mongoose.model('cars');
 const crypto = require('crypto');
 
 router.get('/search', (req, res)=> {
@@ -19,39 +17,54 @@ router.post('/search', (req, res) => {
     input_str = `${locationp}${datep}${timep}${durationp}`;
 
     const id = crypto.createHash('sha256').update(input_str).digest('hex');
-    Car.find({ searchid: id }, (err, cars) => {
+
+    // Connect to the MongoDB database
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = 'mongodb+srv://Lim:Lim123@cluster0.4ejvdiv.mongodb.net/CC_Rental?retryWrites=true&w=majority';
+    MongoClient.connect(uri, function(err, client) {
         if (err) {
             console.log(err);
-            res.status(500).send("Error finding cars");
-        } else if (cars.length === 0) {
-            // If no cars are found with the same ID, start web scraping
-            const scraperProcess = spawn('python', ['../webscraper/scrape.py', locationp, datep, timep, durationp]);
-        
-            // Listen for output from the Python script
-            scraperProcess.stdout.on('data', (data) => {
-                console.log(`Python script output: ${data}`);
-            });
-        
-            // Listen for errors from the Python script
-            scraperProcess.stderr.on('data', (data) => {
-                console.error(`Python script error: ${data}`);
-            });
-        
-            // Listen for the Python script to exit
-            scraperProcess.on('exit', (code) => {
-                console.log(`Python script exited with code ${code}`);
-                // Now the car with searchid wanted is in database
-                Car.find({ searchid: id }, (err, cars) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).send("Error finding cars");
-                    } else {
-                        res.status(200).send(cars.sort((a, b) => a.price - b.price));
-                    }
-                });
-            });
+            res.status(500).send("Error connecting to MongoDB");
         } else {
-            res.status(200).send(cars.sort((a, b) => a.price - b.price));
+            const db = client.db('CC_Rental');
+            const collection = db.collection('cars');
+
+            // Find cars with the specified search ID
+            collection.find({ searchid: id }).toArray(function(err, cars) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send("Error finding cars");
+                } else if (cars.length === 0) {
+                    // If no cars are found with the same ID, start web scraping
+                    const scraperProcess = spawn('python', ['../back-end/webscraper/scrape.py', locationp, datep, timep, durationp]);
+                
+                    // Listen for output from the Python script
+                    scraperProcess.stdout.on('data', (data) => {
+                        console.log(`Python script output: ${data}`);
+                    });
+                
+                    // Listen for errors from the Python script
+                    scraperProcess.stderr.on('data', (data) => {
+                        console.error(`Python script error: ${data}`);
+                    });
+                
+                    // Listen for the Python script to exit
+                    scraperProcess.on('exit', (code) => {
+                        console.log(`Python script exited with code ${code}`);
+                        // Now the car with searchid wanted is in the database
+                        collection.find({ searchid: id }).toArray(function(err, cars) {
+                            if (err) {
+                                console.log(err);
+                                res.status(500).send("Error finding cars");
+                            } else {
+                                res.status(200).send(cars.sort((a, b) => a.price - b.price));
+                            }
+                        });
+                    });
+                } else {
+                    res.status(200).send(cars.sort((a, b) => a.price - b.price));
+                }
+            });
         }
     });
 });
